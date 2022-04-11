7a77a3147b95247e445d78ffd5cb503dbae19aab
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2022-04-11 21:27:38 +0200

    .

--- custom.el
+++ custom.el
@@ -3,11 +3,45 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#292D3E" "#ff5370" "#c3e88d" "#ffcb6b" "#82aaff" "#c792ea" "#89DDFF" "#EEFFFF"])
  '(custom-safe-themes
-   '("47db50ff66e35d3a440485357fb6acb767c100e135ccdf459060407f8baea7b2" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "cbdf8c2e1b2b5c15b34ddb5063f1b21514c7169ff20e081d39cf57ffee89bc1e" "23c806e34594a583ea5bbf5adf9a964afe4f28b4467d28777bcba0d35aa0872e" "da53441eb1a2a6c50217ee685a850c259e9974a8fa60e899d393040b4b8cc922" "a6e620c9decbea9cac46ea47541b31b3e20804a4646ca6da4cce105ee03e8d0e" "d47f868fd34613bd1fc11721fe055f26fd163426a299d45ce69bef1f109e1e71" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" default))
+   '("c414f69a02b719fb9867b41915cb49c853489930be280ce81385ff7b327b4bf6" "b4345da5ff6fbd56e6cdaac9e51194aa759eb44453b8d30fb51778d9b7fa1473" "47db50ff66e35d3a440485357fb6acb767c100e135ccdf459060407f8baea7b2" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "cbdf8c2e1b2b5c15b34ddb5063f1b21514c7169ff20e081d39cf57ffee89bc1e" "23c806e34594a583ea5bbf5adf9a964afe4f28b4467d28777bcba0d35aa0872e" "da53441eb1a2a6c50217ee685a850c259e9974a8fa60e899d393040b4b8cc922" "a6e620c9decbea9cac46ea47541b31b3e20804a4646ca6da4cce105ee03e8d0e" "d47f868fd34613bd1fc11721fe055f26fd163426a299d45ce69bef1f109e1e71" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" default))
+ '(exwm-floating-border-color "#232635")
+ '(fci-rule-color "#676E95")
  '(global-command-log-mode t)
+ '(highlight-tail-colors ((("#383f45") . 0) (("#323e51") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#1c1f2b" "#c792ea"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1c1f2b" "#c3e88d"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1c1f2b" "#676E95"))
+ '(objed-cursor-color "#ff5370")
  '(package-selected-packages
-   '(helpful ivy-rich undo-tree rainbow-mode delight which-key whic-key rainbow-delimiters org-autolist org-autolist-mode org-superstar multiple-cursors adaptive-wrap visual-fill-column org-bullets doom-themes olivetti doom-modeline swipereval- counsel ivy command-log-mode use-package)))
+   '(svg-tag-mode org-modern treemacs-tab-bar treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs autothemer autotheme general helpful ivy-rich undo-tree rainbow-mode delight which-key whic-key rainbow-delimiters org-autolist org-autolist-mode org-superstar multiple-cursors adaptive-wrap visual-fill-column org-bullets doom-themes olivetti doom-modeline swipereval- counsel ivy command-log-mode use-package))
+ '(pdf-view-midnight-colors (cons "#EEFFFF" "#292D3E"))
+ '(rustic-ansi-faces
+   ["#292D3E" "#ff5370" "#c3e88d" "#ffcb6b" "#82aaff" "#c792ea" "#89DDFF" "#EEFFFF"])
+ '(vc-annotate-background "#292D3E")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#c3e88d")
+    (cons 40 "#d7de81")
+    (cons 60 "#ebd476")
+    (cons 80 "#ffcb6b")
+    (cons 100 "#fcb66b")
+    (cons 120 "#f9a16b")
+    (cons 140 "#f78c6c")
+    (cons 160 "#e78e96")
+    (cons 180 "#d690c0")
+    (cons 200 "#c792ea")
+    (cons 220 "#d97dc1")
+    (cons 240 "#ec6898")
+    (cons 260 "#ff5370")
+    (cons 280 "#d95979")
+    (cons 300 "#b36082")
+    (cons 320 "#8d678b")
+    (cons 340 "#676E95")
+    (cons 360 "#676E95")))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
