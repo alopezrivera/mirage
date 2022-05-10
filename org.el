7a1c49eba83fa7801c61a41b239d3374e8f25476
root <root@DESKTOP-DDC7BB5.localdomain>
2022-05-11 01:04:03 +0200

    .

--- org.el
+++ org.el
@@ -920,3 +920,21 @@ folded."
 (setq org-todo-keywords
       '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
 	(sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(r)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
+
+(straight-use-package 'calfw)
+(straight-use-package 'calfw-org)
+(straight-use-package 'calfw-ical)
+
+;; org-agenda configuration is lost otherwise
+(with-eval-after-load 'org-agenda
+  (require 'calfw-org)
+  (require 'calfw-ical))
+
+(defun custom/org-calendar ()
+  "Open `calfw' Org Agenda calendar."
+  (interactive)
+  (require 'org-agenda)
+  (let ((inhibit-message t))
+       (cfw:open-org-calendar)))
+
+(global-set-key (kbd "C-c c") 'custom/org-calendar)
