606dc49eea28c7ce7f0bfd9d53ebb9a0132aa28e
root <root@DESKTOP-DDC7BB5.localdomain>
2022-05-19 12:54:33 +0200

    wild search, mc

--- wild.el
+++ wild.el
@@ -37,12 +37,50 @@
 (modus-themes-load-themes)
 (modus-themes-load-operandi)
 
-;; selectrum
-(straight-use-package 'selectrum)
-(selectrum-mode +1)
-
 ;; magit
 (straight-use-package 'magit)
+
+;; multiple cursors
+(straight-use-package 'multiple-cursors)
+(require 'multiple-cursors)
+
+;; mc-lists
+(setq mc/list-file "~/.emacs.d/mc-lists.el")
+
+;; Create cursors
+(global-set-key (kbd "C-.")         'mc/mark-next-like-this)
+(global-set-key (kbd "C-;")         'mc/mark-previous-like-this)
+(global-set-key (kbd "C-<mouse-1>") 'mc/add-cursor-on-click)
+(global-unset-key [C-down-mouse-1]) ; necessary
+
+;; Return as usual
+(define-key mc/keymap (kbd "<return>")       'electric-newline-and-maybe-indent)
+
+;; Exit multiple-cursors-mode
+(define-key mc/keymap (kbd "<escape>")       'multiple-cursors-mode)
+(define-key mc/keymap (kbd "<mouse-1>")      'multiple-cursors-mode)
+(define-key mc/keymap (kbd "<down-mouse-1>")  nil) ; necessary
+
+;; swiper
+(straight-use-package 'swiper)
+(require 'swiper)
+
+(defun custom/swiper-isearch (orig-fun &rest args)
+  "`swiper-isearch' the selected region. If none are, `swiper-isearch'."
+  (if (region-active-p)
+      (let ((beg (region-beginning))
+	    (end (region-end)))
+	(deactivate-mark)
+	(apply orig-fun (buffer-substring-no-properties beg end)))
+    (apply orig-fun args)))
+(advice-add 'swiper-isearch :around #'custom/swiper-isearch)
+(define-key global-map (kbd "C-s") #'swiper-isearch)
+
+(defun custom/swiper-multiple-cursors ()
+  (interactive)
+  (swiper-mc)
+  (minibuffer-keyboard-quit))
+(define-key swiper-map (kbd "M-<return>") 'custom/swiper-multiple-cursors)
 
 ;; winner
 (winner-mode)
