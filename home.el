61e3e2be0ce95e1017cf4c6b498be7aa2f8a0676
root <root@DESKTOP-DDC7BB5.localdomain>
2022-06-05 16:30:40 +0200

    .

--- home.el
+++ home.el
@@ -775,6 +775,21 @@ kill the current buffer and delete its window."
   (let ((default-directory (or dir (file-name-directory buffer-file-name))))
     (shell-command "find . -maxdepth 1 -type f -exec dos2unix \\{\\} \\;")))
 
+(defun custom/reload-from-disk (&optional buffer)
+  "Revert BUFFER contents to the contents of its
+file saved on disk, ignoring the auto-save file.
+If the buffer has unsaved modifications, prompt
+the user for confirmation."
+  (interactive)
+  (let ((buffer (or buffer (current-buffer))))
+    (save-window-excursion
+      (switch-to-buffer buffer)
+      (if (not (buffer-modified-p))
+	     (revert-buffer t t)
+	   (revert-buffer t nil)))))
+
+(global-set-key (kbd "C-c r") 'custom/reload-from-disk)
+
 (require 'ui (concat config-directory "ui.el"))
 
 (require 'theme (concat config-directory "theme.el"))
