1be0a9c52cef4dbf08ca019d8abb735066e73553
Antonio Lopez Rivera <alopezrivera@VERYHUMAN-2-U.dfki.uni-bremen.de>
2022-06-23 17:43:21 +0200

    wild - ace-window

--- wild.el
+++ wild.el
@@ -57,6 +57,10 @@
 
 ;; winner
 (winner-mode)
+;; ace-window
+(straight-use-package 'ace-window)
+(require 'ace-window)
+(global-set-key (kbd "C-x o") 'ace-window)
 
 ;; workgroups
 (straight-use-package 'workgroups)
