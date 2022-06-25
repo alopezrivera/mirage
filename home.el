61f91fad4d6f8ee0fbdd43edabaa0f123de8e8ed
Antonio López Rivera <anlo0>
2022-06-25 12:09:24 +0200

    .

--- home.el
+++ home.el
@@ -132,7 +132,7 @@ to the query at execution."
   (custom/relative-line-regex "^[[:blank:]]*\\([0-9]+[.\\)]\\{1\\}\\|[-+*]\\{1\\}\\)[[:blank:]]+.*$" number))
 
 (defun custom/relative-line-empty (&optional number)
-  (custom/relative-line-regex "^[[:space:]]+$" number))
+  (custom/relative-line-regex "^[[:space:]]*$" number))
 
 (defun custom/relative-line-wrapped ()
   (> (custom/get-point 'beginning-of-visual-line) (custom/get-point 'beginning-of-line-text)))
