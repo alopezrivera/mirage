c8a132635530079b329979cfcf7a9ece1f979244
alopezrivera <antonlopezr99@gmail.com>
2022-07-09 20:33:29 +0200

    local

---
+++ local/local--antonio-HP-ZBook-Studio-x360-G5.el
@@ -0,0 +1,29 @@
+;; local emacs config
+
+(setq config "home")
+
+(setq home "/media/antonio/T7/")
+
+(setq startup-buffers
+      (list (concat config-directory "backlog.org")
+            (concat home "studio/backlog.org")
+	    (concat home "studio/professional/work/DFKI/repos/hopping_leg/backlog.org")))
+
+
+;; org-agenda
+(setq org-agenda-files (list (concat home "studio/contacts.org")
+			     (concat home "studio/professional/work/DFKI/repos/hopping_leg/backlog.org")))
+
+;; org-contacts
+(setq org-contacts-files (list (concat home "studio/contacts.org")))
+
+;; org-roam directory
+(setq org-roam-directory (concat home "home/roam"))
+
+;; org-diary directory
+(setq custom/org-diary-directory (concat home "home/journal/diary/"))
+
+
+;; projectile
+(setq projectile-project-search-path (list (concat home "studio/")
+				           (concat home "home/")))
