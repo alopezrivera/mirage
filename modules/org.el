705ad4c4e7ee23adb3b1246d653b7180a7cf71e0
alopezrivera <antonlopezr99@gmail.com>
2022-07-28 00:48:11 +0200

    .

--- modules/org.el
+++ modules/org.el
@@ -722,60 +722,6 @@ indented at the level of the previous list item, indent the paragraph."
 ;; symbols, super- and subscripts
 (setq org-pretty-entities nil)
 
-(require 'org-capture)
-
-;; Required as of Org 9.2
-(require 'org-tempo)
-
-;; Navigation
-(global-set-key (kbd "C-<tab>")         'tempo-forward-mark)
-(global-set-key (kbd "C-<iso-lefttab>") 'tempo-backward-mark)
-
-;; equations
-(tempo-define-template "latex-equation"
-		          '("#+NAME: eq:" p n
-			    "\\begin{equation}" n
-			    p n
-			    "\\end{equation}" >)
-			  "<eq"
-			  "LaTeX equation template")
-
-(tempo-define-template "latex-derivation"
-		          '("#+NAME: eq:" p n
-			    "\\begin{equation}" n
-			    "\\arraycolsep=3pt\\def\\arraystretch{2.25}" n
-			    "\\begin{array}{lll}" n
-			    p n
-			    "\\end{array}" n
-			    "\\end{equation}" >)
-			  "<de"
-			  "LaTeX derivation template")
-
-;; figures
-(tempo-define-template "figure"
-		          '("#+NAME: fig:" p n
-			    "#+CAPTION: " p n
-			    "#+ATTR_ORG: :width 450" n
-			    "[[./" p "]]" >)
-			  "<f"
-			  "Org Mode figure template")
-
-(defun custom/tempo-code-block (key language)
-  (tempo-define-template language
-		         `("#+begin_src " ,language n
-			   n
-			   p n
-			   n
-			   "#+end_src" >)
-			 key
-			 language))
-
-(dolist (pair '(("<sh"   "shell")
-		   ("<el"   "emacs-lisp")
-		   ("<py"   "python")
-		   ("<bash" "bash")))
-  (apply 'custom/tempo-code-block pair))
-
 (defun custom/org-end ()
   "Conditional end in Org Mode.
 
@@ -898,6 +844,77 @@ If `org-at-table-p', home to `org-table-beginning-of-field'."
       ;; else, attempt going to last subheading of previous same-level heading
       (custom/org-goto-child-last))))
 
+;; titles
+(setq typeface-title "Ringbearer")
+
+;; heading face
+(setq typeface-heading "Century Gothic")
+
+;; title face
+
+(defun custom/org-typeface-title ()
+  (with-eval-after-load 'org-faces
+    (set-face-attribute 'org-document-title nil :font typeface-title :height 300 :weight 'regular :foreground 'unspecified)))
+
+(add-hook 'org-mode-hook #'custom/org-typeface-title)
+
+(defun custom/org-typefaces-body ()
+  (with-eval-after-load 'org-faces
+
+    ;; Code
+    (set-face-attribute 'org-block                 nil :foreground nil :inherit 'fixed-pitch)
+    (set-face-attribute 'org-code                  nil                 :inherit '(shadow fixed-pitch))
+    (set-face-attribute 'org-verbatim              nil                 :inherit '(shadow fixed-pitch))
+
+    ;; Tables
+    (set-face-attribute 'org-table                 nil                 :inherit '(shadow fixed-pitch))
+
+    ;; Lists
+    (set-face-attribute 'org-checkbox              nil                 :inherit 'fixed-pitch)
+
+    ;; Meta
+    (set-face-attribute 'org-meta-line             nil                 :inherit 'fixed-pitch)
+    (set-face-attribute 'org-document-info         nil                 :inherit 'fixed-pitch)
+    (set-face-attribute 'org-document-info-keyword nil                 :inherit 'fixed-pitch)
+    (set-face-attribute 'org-special-keyword       nil                 :inherit 'fixed-pitch)))
+
+(add-hook 'org-mode-hook #'custom/org-typefaces-body)
+
+(defun custom/org-typeface-indent ()
+  "Indent typeface used in `org-indent-mode' and `visual-line-mode'."
+  (with-eval-after-load 'org-indent-mode
+    (set-face-attribute 'org-indent                nil                 :inherit '(org-hide fixed-pitch))))
+
+(add-hook 'org-mode-hook #'custom/org-typeface-indent)
+
+;; use levels 1 through 8
+(setq org-n-level-faces 8)
+
+;; do not cycle header style after 8th level
+(setq org-cycle-level-faces nil)
+
+;; hide leading stars
+(setq org-hide-leading-starts t)
+
+;; font sizes
+(defun custom/org-header-setup () 
+  (with-eval-after-load 'org-faces
+
+    ;; heading font sizes
+    (dolist (face '((org-level-1 . 1.6)
+                    (org-level-2 . 1.4)
+                    (org-level-3 . 1.3)
+                    (org-level-4 . 1.2)
+                    (org-level-5 . 1.15)
+                    (org-level-6 . 1.10)
+                    (org-level-7 . 1.05)
+                    (org-level-8 . 1.00)))
+         (set-face-attribute (car face) nil :font typeface-heading :weight 'bold :height (cdr face)))))
+
+(add-hook 'org-mode-hook #'custom/org-header-setup)
+
+(add-hook 'org-mode-hook (lambda () (org-indent-mode 1)))
+
 (setq org-hide-emphasis-markers t)
 
 (setq org-hidden-keywords '(title))
@@ -919,12 +936,7 @@ If `org-at-table-p', home to `org-table-beginning-of-field'."
 (setq org-appear-autosubmarkers t)
 (setq org-appear-inside-latex t)
 
-(add-hook 'org-mode-hook (lambda () (org-indent-mode 1)))
-
 (add-hook 'org-mode-hook (lambda () (progn (visual-line-mode 1) (setq line-move-visual t))))
-
-;; Change ellipsis ("...") to remove clutter
-(setq org-ellipsis " ♢")
 
 (straight-use-package 'org-modern)
 
@@ -949,9 +961,8 @@ If `org-at-table-p', home to `org-table-beginning-of-field'."
 ;; Priorities
 (setq org-modern-priority nil)
 
-;; ox-rst
-(straight-use-package 'ox-rst)
-(require 'ox-rst)
+;; Change ellipsis ("...") to remove clutter
+(setq org-ellipsis " ♢")
 
 (setq org-format-latex-options
         (list :foreground 'default
@@ -1047,6 +1058,64 @@ matches the current theme."
 (straight-use-package 'org-fragtog)
 
 (add-hook 'org-mode-hook #'org-fragtog-mode)
+
+;; ox-rst
+(straight-use-package 'ox-rst)
+(require 'ox-rst)
+
+;; Required as of Org 9.2
+(require 'org-tempo)
+
+;; Navigation
+(global-set-key (kbd "C-<tab>")         'tempo-forward-mark)
+(global-set-key (kbd "C-<iso-lefttab>") 'tempo-backward-mark)
+
+;; equations
+(tempo-define-template "latex-equation"
+		          '("#+NAME: eq:" p n
+			    "\\begin{equation}" n
+			    p n
+			    "\\end{equation}" >)
+			  "<eq"
+			  "LaTeX equation template")
+
+(tempo-define-template "latex-derivation"
+		          '("#+NAME: eq:" p n
+			    "\\begin{equation}" n
+			    "\\arraycolsep=3pt\\def\\arraystretch{2.25}" n
+			    "\\begin{array}{lll}" n
+			    p n
+			    "\\end{array}" n
+			    "\\end{equation}" >)
+			  "<de"
+			  "LaTeX derivation template")
+
+;; figures
+(tempo-define-template "figure"
+		          '("#+NAME: fig:" p n
+			    "#+CAPTION: " p n
+			    "#+ATTR_ORG: :width 450" n
+			    "[[./" p "]]" >)
+			  "<f"
+			  "Org Mode figure template")
+
+(defun custom/tempo-code-block (key language)
+  (tempo-define-template language
+		         `("#+begin_src " ,language n
+			   n
+			   p n
+			   n
+			   "#+end_src" >)
+			 key
+			 language))
+
+(dolist (pair '(("<sh"   "shell")
+		   ("<el"   "emacs-lisp")
+		   ("<py"   "python")
+		   ("<bash" "bash")))
+  (apply 'custom/tempo-code-block pair))
+
+(require 'org-capture)
 
 (require 'org-diary (concat config-directory "packages/org-diary.el"))
 
--- modules/ui.el
+++ modules/ui.el
@@ -1,13 +1,13 @@
-;; Default face
+;; default face
 (set-face-attribute 'default nil        :font "Fira Code Retina" :height 93)
 
-;; Fixed pitch face
+;; fixed pitch face
 (set-face-attribute 'fixed-pitch nil    :font "Fira Code Retina" :height 93)
 
-;; Variable pitch face
-(set-face-attribute 'variable-pitch nil :font "PT Sans"  :height 100 :weight 'regular)
+;; variable pitch face
+(set-face-attribute 'variable-pitch nil :font "PT Sans"  :height 105 :weight 'regular)
 
-;; Italic
+;; italic
 (defface custom/italic
   '((t :font "Victor Mono" :height  86 :weight  bold :slant italic))
   "Italic typeface")
@@ -37,80 +37,11 @@ through as a theme loads."
 
 (advice-add 'load-theme :around #'custom/theme-accents)
 
-;; Titles
-(setq typeface-title "Ringbearer")
-
-;; Heading face
-(setq typeface-heading "Century Gothic")
-
 ;; Mode line
 (set-face-attribute 'mode-line nil :height 85 :inherit 'fixed-pitch)
 
 ;; Symbol library
 (straight-use-package 'all-the-icons)
-
-;; Title face
-
-(defun custom/org-typeface-title ()
-  (with-eval-after-load 'org-faces
-    (set-face-attribute 'org-document-title nil :font typeface-title :height 300 :weight 'regular :foreground 'unspecified)))
-
-(add-hook 'org-mode-hook #'custom/org-typeface-title)
-
-(defun custom/org-typefaces-body ()
-  (with-eval-after-load 'org-faces
-
-    ;; Code
-    (set-face-attribute 'org-block                 nil :foreground nil :inherit 'fixed-pitch)
-    (set-face-attribute 'org-code                  nil                 :inherit '(shadow fixed-pitch))
-    (set-face-attribute 'org-verbatim              nil                 :inherit '(shadow fixed-pitch))
-
-    ;; Tables
-    (set-face-attribute 'org-table                 nil                 :inherit '(shadow fixed-pitch))
-
-    ;; Lists
-    (set-face-attribute 'org-checkbox              nil                 :inherit 'fixed-pitch)
-
-    ;; Meta
-    (set-face-attribute 'org-meta-line             nil                 :inherit 'fixed-pitch)
-    (set-face-attribute 'org-document-info         nil                 :inherit 'fixed-pitch)
-    (set-face-attribute 'org-document-info-keyword nil                 :inherit 'fixed-pitch)
-    (set-face-attribute 'org-special-keyword       nil                 :inherit 'fixed-pitch)))
-
-(add-hook 'org-mode-hook #'custom/org-typefaces-body)
-
-(defun custom/org-typeface-indent ()
-  "Indent typeface used in `org-indent-mode' and `visual-line-mode'."
-  (with-eval-after-load 'org-indent-mode
-    (set-face-attribute 'org-indent                nil                 :inherit '(org-hide fixed-pitch))))
-
-(add-hook 'org-mode-hook #'custom/org-typeface-indent)
-
-;; Use levels 1 through 8
-(setq org-n-level-faces 8)
-
-;; Do not cycle header style after 8th level
-(setq org-cycle-level-faces nil)
-
-;; Hide leading stars
-(setq org-hide-leading-starts t)
-
-;; Font sizes
-(defun custom/org-header-setup () 
-  (with-eval-after-load 'org-faces
-
-    ;; Heading font sizes
-    (dolist (face '((org-level-1 . 1.6)
-                    (org-level-2 . 1.4)
-                    (org-level-3 . 1.3)
-                    (org-level-4 . 1.2)
-                    (org-level-5 . 1.15)
-                    (org-level-6 . 1.10)
-                    (org-level-7 . 1.05)
-                    (org-level-8 . 1.00)))
-         (set-face-attribute (car face) nil :font typeface-heading :weight 'bold :height (cdr face)))))
-
-(add-hook 'org-mode-hook #'custom/org-header-setup)
 
 (straight-use-package 'svg-tag-mode)
 
