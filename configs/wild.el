97b2d6f8733a6018bc5e059220b831d072377391
alopezrivera <antonlopezr99@gmail.com>
2022-08-02 18:14:17 +0200

    wild - rewrite

--- configs/wild.el
+++ configs/wild.el
@@ -1,43 +1,36 @@
 ;;; -*- lexical-binding: t; -*-
 
-;; @@@  @@@  @@@  @@@  @@@       @@@@@@@      @@@@@@@@  @@@@@@@@@@    @@@@@@    @@@@@@@   @@@@@@   A p
-;; @@@  @@@  @@@  @@@  @@@       @@@@@@@@     @@@@@@@@  @@@@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@   n e
-;; @@!  @@!  @@!  @@!  @@!       @@!  @@@     @@!       @@! @@! @@!  @@!  @@@  !@@       !@@       t z
-;; !@!  !@!  !@!  !@!  !@!       !@!  @!@     !@!       !@! !@! !@!  !@!  @!@  !@!       !@!       o 
-;; @!!  !!@  @!@  !!@  @!!       @!@  !@!     @!!!:!    @!! !!@ @!@  @!@!@!@!  !@!       !!@@!!    n R
-;; !@!  !!!  !@!  !!!  !!!       !@!  !!!     !!!!!:    !@!   ! !@!  !!!@!!!!  !!!        !!@!!!   i i
-;; !!:  !!:  !!:  !!:  !!:       !!:  !!!     !!:       !!:     !!:  !!:  !!!  :!!            !:!  o v
-;; :!:  :!:  :!:  :!:   :!:      :!:  !:!     :!:       :!:     :!:  :!:  !:!  :!:           !:!     e
-;;  :::: :: :::    ::   :: ::::   :::: ::      :: ::::  :::     ::   ::   :::   ::: :::  :::: ::   L r
-;;   :: :  : :    :    : :: : :  :: :  :      : :: ::    :      :     :   : :   :: :: :  :: : :    ó a
+;;; Commentary
 
-;; display
-(setq-default frame-title-format '("Emacs [%m] %b"))
-(setq inhibit-startup-message t)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(tooltip-mode -1)
-(menu-bar-mode -1)
-(tab-bar-mode 1)
+;; Minimal Emacs Config
+;; ====================
+;; Contents
+;; --------
+;; Setup
+;;   - Infrastructure
+;;   - Startup
+;; Looks
+;;   - UI
+;;   - Themes
+;; Niceties
+;;   - Navigation
+;;   - Session
+;;   - Editing
+;;   - Completion
+;;   - Search
+;; Functions
+;;   - IDE
+;;   - Org Mode
+;;   - Version Control
+;; --------------------
 
-;; warnings
-(setq visible-bell t)
+;;; Code
 
-;; typefaces
-(set-face-attribute 'default     nil :height 85)
+;; ===================
+;;   Infrastructure
+;; ===================
 
-;; dialogues
-(advice-add 'yes-or-no-p :override #'y-or-n-p)
-
-;; line numbers
-(global-set-key (kbd "C-c l") #'display-line-numbers-mode)
-
-;; no tabs
-(setq-default indent-tabs-mode nil)
-
-;; Package manager =============================================================
-
-;; straight.el
+;; package manager
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -51,37 +44,67 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-;; IDE =========================================================================
+;; ===================
+;;      Startup
+;; ===================
 
-(require 'ide (concat config-directory "ide.el"))
+(setq inhibit-startup-message t)
 
-;; shell =======================================================================
+(defun spawn-startup-buffers ()
+  (cl-loop for buffer in startup-buffers
+	   collect (find-file-noselect buffer)))
+(add-hook 'after-init-hook #'spawn-startup-buffers)
 
-;; remove duplicates in shell history
-(setq comint-input-ignoredups t)
+;; ===================
+;;        UI
+;; ===================
 
-;; Emacs packages ==============================================================
+;; display
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(menu-bar-mode -1)
+(tab-bar-mode 1)
 
-;; winner
-(winner-mode)
+(setq-default frame-title-format '("Emacs [%m] %b"))
 
-;; desktop
-(desktop-save-mode 1)
+;; warnings
+(setq visible-bell t)
+(advice-add 'yes-or-no-p :override #'y-or-n-p)
+
+;; typefaces
+(set-face-attribute 'default nil :height 85)
+
+;; guides
+(global-set-key (kbd "C-c l") #'display-line-numbers-mode)
+
+;; ===================
+;;       Themes
+;; ===================
 
 ;; modus
 (straight-use-package 'modus-themes)
 (modus-themes-load-themes)
 (modus-themes-load-operandi)
 
-;; External packages ===========================================================
+;; ===================
+;;      Navigation
+;; ===================
 
-;; magit
-(straight-use-package 'magit)
+;; winner
+(winner-mode)
 
 ;; ace-window
 (straight-use-package 'ace-window)
 (require 'ace-window)
 (global-set-key (kbd "C-x o") 'ace-window)
+
+;; ===================
+;;       Session
+;; ===================
+
+;; desktop
+(desktop-save-mode 1)
 
 ;; workgroups
 (straight-use-package 'workgroups)
@@ -89,49 +112,36 @@
 (setq wg-prefix-key (kbd "C-c w"))
 (workgroups-mode 1)
 
-;; treemacs
-(straight-use-package 'treemacs)
-(require 'treemacs)
+;; ===================
+;;       Editing
+;; ===================
 
-(global-set-key (kbd "C-x t t") 'treemacs)
+;; tabs
+(setq-default indent-tabs-mode nil)
 
-(defvar custom/treemacs-ignored '(".*__pycache__.*")
-  "Files and directories ignored by treemacs")
-
-(defun custom/treemacs-ignore-filter (file _)
-  (cl-loop for ignored in custom/treemacs-ignored
-	   if (string-match ignored file)
-	      return t
-	   finally return nil))
-(push #'custom/treemacs-ignore-filter treemacs-ignored-file-predicates)
-
-;; multiple cursors ------------------------------------------------------------
+;; multiple cursors
 (straight-use-package 'multiple-cursors)
 (require 'multiple-cursors)
 
-;; mc-lists
 (setq mc/list-file "~/.emacs.d/persistent/mc-lists.el")
-
-;; Create cursors
-(global-set-key (kbd "C-.")         'mc/mark-next-like-this)
-(global-set-key (kbd "C-;")         'mc/mark-previous-like-this)
-(global-set-key (kbd "C-<mouse-1>") 'mc/add-cursor-on-click)
+(global-set-key (kbd "C-.")                  'mc/mark-next-like-this)
+(global-set-key (kbd "C-;")                  'mc/mark-previous-like-this)
+(global-set-key (kbd "C-<mouse-1>")          'mc/add-cursor-on-click)
 (global-unset-key [C-down-mouse-1]) ; necessary
-
-;; Return as usual
 (define-key mc/keymap (kbd "<return>")       'electric-newline-and-maybe-indent)
-
-;; Exit multiple-cursors-mode
 (define-key mc/keymap (kbd "<escape>")       'multiple-cursors-mode)
 (define-key mc/keymap (kbd "<mouse-1>")      'multiple-cursors-mode)
 (define-key mc/keymap (kbd "<down-mouse-1>")  nil) ; necessary
 
-;; ivy -------------------------------------------------------------------------
+;; ===================
+;;     Completion
+;; ===================
+
+;; ivy
 (straight-use-package 'ivy)
 (require 'ivy)
 (ivy-mode 1)
 
-;; minibuffer bindings
 (let ((map ivy-minibuffer-map))
   (cl-loop for binding in '(("<tab>"       . ivy-alt-done)
 			    ("<up>"        . ivy-previous-line-or-history)
@@ -140,19 +150,23 @@
 			    ("C-k"         . ivy-previous-line)
 			    ("<backspace>" . ivy-backward-delete-char))
             collect (define-key map (kbd (car binding)) (cdr binding))))
-;; switch-buffer bindings
+
 (let ((map ivy-switch-buffer-map))
   (cl-loop for binding in '(("C-k"   . ivy-previous-line)
  			    ("C-l"   . ivy-done)
 			    ("C-d"   . ivy-switch-buffer-kill))
             collect (define-key map (kbd (car binding)) (cdr binding))))
-;; reverse-i-search bindings
+
 (let ((map ivy-reverse-i-search-map))
   (cl-loop for binding in '(("C-k"   . ivy-previous-line)
 			    ("C-d"   . ivy-reverse-i-search-kill))
             collect (define-key map (kbd (car binding)) (cdr binding))))
 
-;; swiper ----------------------------------------------------------------------
+;; ===================
+;;       Search
+;; ===================
+
+;; swiper
 (straight-use-package 'swiper)
 (require 'swiper)
 
@@ -173,7 +187,57 @@
   (minibuffer-keyboard-quit))
 (define-key swiper-map (kbd "M-<return>") 'custom/swiper-multiple-cursors)
 
-;; org -------------------------------------------------------------------------
+;; ===================
+;;         IDE
+;; ===================
+
+;; remove duplicates in shell history
+(setq comint-input-ignoredups t)
+
+;; file tree
+(straight-use-package 'treemacs)
+(require 'treemacs)
+(global-set-key (kbd "C-x t t") 'treemacs)
+
+(defvar custom/treemacs-ignored '(".*__pycache__.*")
+  "Files, directories and patterns ignored by treemacs.")
+(defun custom/treemacs-ignore-filter (file _)
+  "If FILE matches any entry of `treemacs-ignored', return t."
+  (cl-loop for ignored in custom/treemacs-ignored
+	   if (string-match ignored file)
+	      return t
+	   finally return nil))
+(push #'custom/treemacs-ignore-filter treemacs-ignored-file-predicates)
+
+;; completion
+(straight-use-package 'company)
+(require 'company)
+
+;; syntax checker
+(straight-use-package 'flycheck)
+(require 'flycheck)
+(add-hook 'prog-mode-hook #'flycheck-mode)
+
+;; lisp
+(straight-use-package 'rainbow-delimiters)
+(require 'rainbow-delimiters)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+;; python
+(straight-use-package 'elpy)
+(elpy-enable)
+
+(setq elpy-rpc-timeout 5)
+(setq elpy-rpc-backend "jedi")
+(setq elpy-rpc-python-command "python3")
+
+(define-key elpy-mode-map (kbd "C-M-n") 'elpy-nav-forward-block)
+(define-key elpy-mode-map (kbd "C-M-p") 'elpy-nav-backward-block)
+
+;; ===================
+;;       Org Mode
+;; ===================
+
 (setq org-src-tab-acts-natively        t)
 (setq org-src-preserve-indentation     nil)
 (setq org-edit-src-content-indentation 0)
@@ -197,17 +261,12 @@
 (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
 (add-to-list 'org-structure-template-alist '("py" . "src python"))
 
-;; startup buffers =============================================================
-(defvar background-buffers
-  (list (concat config-directory "local.el")
-        (concat config-directory "init.el")
-        (concat config-directory "wild.el")))
+;; ===================
+;;   Version Control
+;; ===================
 
-(defun custom/spawn-startup-buffers ()
-  (cl-loop for buffer in (append startup-buffers background-buffers)
-	   collect (find-file-noselect buffer)))
-
-(add-hook 'after-init-hook #'custom/spawn-startup-buffers)
-;; =============================================================================
+;; magit
+(straight-use-package 'magit)
 
 (provide 'wild)
+;;; wild.el ends here
