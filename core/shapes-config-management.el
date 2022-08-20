89bc75062dac74d23ce5cacee76d025279441399
alopezrivera <antonlopezr99@gmail.com>
2022-08-20 23:49:06 +0200

    shapes - tangling -> config management

--- core/shapes-config-management.el
+++ core/shapes-config-management.el
@@ -1,3 +1,45 @@
+(defun custom/shapes-tangle (org-file &optional quiet)
+  "Asynchronously tangle an org file."
+  (let ((init-tangle-start-time (current-time))
+	    (file (buffer-file-name))
+	    (async-quiet-switch "-q"))
+    (async-start
+     `(lambda ()
+	      (require 'org)
+	      (add-hook 'org-babel-post-tangle-hook
+			(lambda ()
+				(string-match "\\(^.*/\\)\\(.*\\)\\(/shapes-\\)\\(.*\\)\\(.el\\)" (buffer-file-name))
+				(let ((component (match-string 4 (buffer-file-name)))
+				      (comp-type (match-string 2 (buffer-file-name))))
+				     (end-of-buffer)
+				     (insert (concat
+					      "\n"
+					      "(provide 'shapes-" (if (string-match-p "^.*s" comp-type) (substring comp-type 0 -1) comp-type) "-" component ")\n"
+					      ";;; shapes-" component ".el ends here"))
+				     (save-buffer))))
+		   (org-babel-tangle-file ,org-file))
+     (unless quiet
+       `(lambda (result)
+		     (if result
+			 (message "SUCCESS: %s successfully tangled (%.2fs)."
+				  ,(file-name-nondirectory org-file)
+				  (float-time (time-subtract (current-time)
+							     ',init-tangle-start-time)))
+		       (message "ERROR: %s tangling failed." ,org-file)))))))
+
+(defun custom/shapes-tangle-auto ()
+  "Automatically tangle Org Mode files in the Emacs config directory"
+  (let* ((file   (expand-file-name buffer-file-name))
+	    (source (string-match (concat config-directory ".*.org$") file))
+	    (shapes (string-match (concat config-directory "shapes.org$") buffer-file-name))
+	    (org-confirm-babel-evaluate nil))
+    (if source
+	(if shapes
+	    (custom/shapes-tangle file)
+	  (org-babel-tangle)))))
+
+(add-hook 'after-save-hook #'custom/shapes-tangle-auto)
+
 (shapes-module "no-littering")
 
 (shapes-module "el-patch")
--- core/shapes-modules.el
+++
@@ -1,8 +0,0 @@
-;; package manager
-(shapes-module "straight")
-
-;; other modules
-(shapes-module "async")
-
-(provide 'shapes-core-modules)
-;;; shapes-modules.el ends here
