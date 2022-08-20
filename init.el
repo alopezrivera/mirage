988f83622e48adb7e5884c75cb63ec80aff192a2
alopezrivera <antonlopezr99@gmail.com>
2022-08-20 23:35:33 +0200

    shapes init - core component list

--- init.el
+++ init.el
@@ -10,13 +10,16 @@
 ;; shapes core
 (add-to-list 'load-path (concat config-directory "core/"))
 
-(let ((components (mapcar
-		   (lambda (source)
-		     (string-match "\\(^.*/shapes-\\)\\(.*\\)\\(.el\\)" source)
-		     (match-string 2 source))
-		   (file-expand-wildcards (concat config-directory "core/*.el")))))
-  (mapc (lambda (component) (require (intern (concat "shapes-core-" component)) (concat "shapes-" component))) components)
-  (message "Shapes: core loaded"))
+(defvar shapes-core-components '("load"
+                                 "tangling"
+                                 "extensions"
+                                 "package-manager"
+                                 "config-management"))
+
+(mapc (lambda (component) (require (intern (concat "shapes-core-" component)) (concat "shapes-" component)))
+      shapes-core-components)
+
+(message "Shapes: core loaded")
 
 (load-file (concat config-directory "configs/" (concat config ".el")))
 
