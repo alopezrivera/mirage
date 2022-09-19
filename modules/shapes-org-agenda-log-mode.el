ed8fa20975fc661c1928b3cbd6d3a480d1c0d151
alopezrivera <antonlopezr99@gmail.com>
2022-09-19 17:09:37 +0200

    calfw, time-tracking separated from org-agenda

---
+++ modules/shapes-org-agenda-log-mode.el
@@ -0,0 +1,7 @@
+;; Org Agenda log mode
+(setq org-agenda-start-with-log-mode t)
+(setq org-log-done 'time)
+(setq org-log-into-drawer t)
+
+(provide 'shapes-module-org-agenda-log-mode)
+;;; shapes-org-agenda-log-mode.el ends here
