879da16943b554698a3fdf48a8bbda27fb9f1b6f
alopezrivera <antonlopezr99@gmail.com>
2022-09-22 12:47:13 +0200

    enabled advanced commands -> input layer

--- layers/shapes-input.el
+++ layers/shapes-input.el
@@ -4,5 +4,8 @@
 ;; confirmation
 (advice-add 'yes-or-no-p :override #'y-or-n-p)
 
+;; advanced commands
+(put 'narrow-to-region 'disabled nil)
+
 (provide 'shapes-layer-input)
 ;;; shapes-input.el ends here
