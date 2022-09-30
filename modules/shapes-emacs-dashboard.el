cae730d6c80033d46c99210c1339a55ccd92fb75
alopezrivera <antonlopezr99@gmail.com>
2022-09-30 11:22:51 +0200

    emacs-dashboard startup, options

--- modules/shapes-emacs-dashboard.el
+++ modules/shapes-emacs-dashboard.el
@@ -7,7 +7,13 @@
 
 (dashboard-setup-startup-hook)
 
+;; options
 (setq dashboard-center-content t)
+(setq dashboard-set-file-icons t)
+
+;; initial buffer choice
+(if (not initial-buffer-choice)
+    (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*"))))
 
 (provide 'shapes-module-emacs-dashboard)
 ;;; shapes-emacs-dashboard.el ends here
