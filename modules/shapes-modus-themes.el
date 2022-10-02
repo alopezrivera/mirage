aff14baebb4eee196edcc1450889f23c0ed9becc
alopezrivera <antonlopezr99@gmail.com>
2022-10-02 15:58:11 +0200

    modus themes prelude

--- modules/shapes-modus-themes.el
+++ modules/shapes-modus-themes.el
@@ -1,8 +1,8 @@
-(straight-use-package 'modus-themes)
-(modus-themes-load-themes)
-
 (setq modus-themes-headings
       '((0 . (monochrome))))
+
+(straight-use-package 'modus-themes)
+(modus-themes-load-themes)
 
 (provide 'shapes-module-modus-themes)
 ;;; shapes-modus-themes.el ends here
