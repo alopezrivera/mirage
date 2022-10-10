cad80bc48f4f37fdfa8e132951ab8c2dbc6992c5
alopezrivera <antonlopezr99@gmail.com>
2022-10-10 15:45:57 +0200

    scrolling settings -> navigation layer

--- layers/shapes-navigation.el
+++ layers/shapes-navigation.el
@@ -1,5 +1,10 @@
-;; bindings
-(global-set-key (kbd "C-S-n") #'make-frame-command)
+;; scrolling
+(setq scroll-step 1)
+(setq scroll-conservatively 10000)
+(setq auto-window-vscroll nil)
+
+(setq mouse-wheel-scroll-amount '(2 ((shift) . 10) ((control) . nil)))
+(setq mouse-wheel-progressive-speed nil)
 
 ;; modules
 (shapes-module "winner")
@@ -7,6 +12,9 @@
 
 ;; extensions
 (shapes-extend "navigation")
+
+;; bindings
+(global-set-key (kbd "C-S-n") #'make-frame-command)
 
 (provide 'shapes-layer-navigation)
 ;;; shapes-navigation.el ends here
--- modules/shapes-pixel-scroll-precision.el
+++
@@ -1,6 +0,0 @@
-(setq scroll-step 1)
-(setq scroll-conservatively 101)
-(setq scroll-preserve-screen-position 1)
-
-(provide 'shapes-module-pixel-scroll-precision)
-;;; shapes-pixel-scroll-precision.el ends here
