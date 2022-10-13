99bfbd50e369521d9b717abbc5842f09332a8348
alopezrivera <antonlopezr99@gmail.com>
2022-10-13 23:45:39 +0200

    org ui and typesetting split for performance reasons

--- layers/shapes-org-ui.el
+++ layers/shapes-org-ui.el
@@ -1,5 +1,3 @@
-(shapes-module "org-modern")
-
 (shapes-module "org-appear")
 
 ;; title face
@@ -9,28 +7,6 @@
 
 (add-hook 'org-mode-hook #'custom/org-typeface-title)
 
-(defun custom/org-typefaces-body ()
-  (with-eval-after-load 'org-faces
-
-    ;; Code
-    (set-face-attribute 'org-block                 nil :inherit 'fixed-pitch)
-    (set-face-attribute 'org-code                  nil :inherit '(shadow fixed-pitch))
-    (set-face-attribute 'org-verbatim              nil :inherit '(shadow fixed-pitch))
-
-    ;; Tables
-    (set-face-attribute 'org-table                 nil :inherit '(shadow fixed-pitch))
-
-    ;; Lists
-    (set-face-attribute 'org-checkbox              nil :inherit 'fixed-pitch)
-
-    ;; Meta
-    (set-face-attribute 'org-meta-line             nil :inherit 'fixed-pitch)
-    (set-face-attribute 'org-document-info         nil :inherit 'fixed-pitch)
-    (set-face-attribute 'org-document-info-keyword nil :inherit 'fixed-pitch)
-    (set-face-attribute 'org-special-keyword       nil :inherit 'fixed-pitch)))
-
-(add-hook 'org-mode-hook #'custom/org-typefaces-body)
-
 (defun custom/org-typeface-indent ()
   "Indent typeface used in `org-indent-mode' and `visual-line-mode'."
   (with-eval-after-load 'org-indent-mode
@@ -38,45 +14,8 @@
 
 (add-hook 'org-mode-hook #'custom/org-typeface-indent)
 
-;; use levels 1 through 8
-(setq org-n-level-faces 8)
-
-;; do not cycle header style after 8th level
-(setq org-cycle-level-faces nil)
-
-;; hide leading stars
-(setq org-hide-leading-starts t)
-
-;; font sizes
-(defun custom/org-header-setup () 
-  (with-eval-after-load 'org-faces
-
-    ;; heading font sizes
-    (dolist (face '((org-level-1 . 1.175)
-                    (org-level-2 . 1.175)
-                    (org-level-3 . 1.175)
-                    (org-level-4 . 1.175)
-                    (org-level-5 . 1.175)
-                    (org-level-6 . 1.175)
-                    (org-level-7 . 1.175)
-                    (org-level-8 . 1.175)))
-         (set-face-attribute (car face) nil :font typeface-heading :weight 'bold :height (cdr face)))))
-
-(add-hook 'org-mode-hook #'custom/org-header-setup)
-
-;; org-indent-mode
-(setq org-startup-indented t)
-
 ;; list indentation
 (setq-default org-list-indent-offset 1)
-
-(add-hook 'org-mode-hook (lambda () (progn (visual-line-mode 1) (setq line-move-visual t))))
-
-;; symbols, super- and subscripts
-(setq org-pretty-entities nil)
-
-;; Change ellipsis ("...") to remove clutter
-(setq org-ellipsis " ♢")
 
 ;; startup with inline images
 (setq org-startup-with-inline-images t)
