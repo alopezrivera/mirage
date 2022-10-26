ee6f1ca859e28b4dff2803351f312f7db1e4da89
alopezrivera <antonlopezr99@gmail.com>
2022-10-26 18:08:53 +0200

    load-session default

--- extensions/shapes-session.el
+++ extensions/shapes-session.el
@@ -1,6 +1,10 @@
 (defun custom/load-session ()
   (interactive)
-  (let ((session (read-file-name "Select session: " "~/sessions/")))
+  (let ((session (read-file-name "Select session: "
+                                 "~/sessions/"
+                                 nil
+                                 nil
+                                 (car (directory-files "~/sessions/" nil "\\`[^.]*\\'")))))
     (desktop-change-dir session)
     (wg-load (concat session ".wg"))
     (call-interactively 'wg-switch-to-workgroup)))
