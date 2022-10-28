9dbb2f94965d47abb8e9bc59f2fb1ca625f98720
alopezrivera <antonlopezr99@gmail.com>
2022-10-28 11:27:09 +0200

    :ARCHIVE:

--- modules/shapes-command-log-mode.el
+++
@@ -1,7 +0,0 @@
-(straight-use-package 'command-log-mode)
-(require 'command-log-mode)
-
-(global-command-log-mode)
-
-(provide 'shapes-module-command-log-mode)
-;;; shapes-command-log-mode.el ends here
--- modules/shapes-ivy-rich.el
+++
@@ -1,7 +0,0 @@
-(straight-use-package 'ivy-rich)
-(require 'ivy-rich)
-
-(ivy-rich-mode 1)
-
-(provide 'shapes-module-ivy-rich)
-;;; shapes-ivy-rich.el ends here
