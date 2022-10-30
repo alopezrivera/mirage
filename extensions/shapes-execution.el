742518dbdb494870a656efb45b9009b29a3f0ea1
alopezrivera <antonlopezr99@gmail.com>
2022-10-30 17:44:12 +0100

    m

--- extensions/shapes-execution.el
+++ extensions/shapes-execution.el
@@ -4,8 +4,9 @@
   (let ((buffers (or buffers (buffer-list))))
     `(cl-loop for buffer in ',buffers
               do (if (buffer-live-p buffer)
-                          (save-window-excursion (switch-to-buffer buffer)
-                                                 ,command)))))
+                     (save-window-excursion
+                       (switch-to-buffer buffer)
+                       ,command)))))
 
 (provide 'shapes-extension-execution)
 ;;; shapes-execution.el ends here
