6930c83fd24ee2839deb5489e094aaa001525651
alopezrivera <antonlopezr99@gmail.com>
2022-11-01 17:08:03 +0100

    catpuccin themes repo name change

--- modules/shapes-catppuccin-themes.el
+++ modules/shapes-catppuccin-themes.el
@@ -1,9 +1,8 @@
-(straight-use-package '(catppuccin-themes :type git :host github :repo "catppuccin/emacs"))
-
 (flet ((repo (name) (concat config-directory "straight/repos/" name)))
-  ;; rename clone directory to "catppuccin-themes"
-  (if (file-exists-p (concat config-directory "straight/repos/emacs/catppuccin-theme.el"))
-      (progn (copy-directory (repo "emacs")
+  ;; install and rename repository directory to "catppuccin-themes"
+  (if (not (file-exists-p (repo "catppuccin-themes/catppuccin-theme.el")))
+      (progn (straight-use-package '(catppuccin-themes :type git :host github :repo "catppuccin/emacs"))
+             (copy-directory (repo "emacs")
                              (repo "catppuccin-themes"))
              (delete-directory (repo "emacs") t)))
   (require 'catppuccin-theme           (repo "catppuccin-themes/catppuccin-theme.el"))
