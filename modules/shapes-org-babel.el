8f3b77927dbd38e62f787168b36b034b35db14bd
alopezrivera <antonlopezr99@gmail.com>
2022-11-23 16:52:48 +0100

    org-babel default header args

--- modules/shapes-org-babel.el
+++ modules/shapes-org-babel.el
@@ -65,22 +65,15 @@ folded."
 ;; Make code indentation reasonable
 (setq org-src-tab-acts-natively        t)
 
-;; Language packages
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((emacs-lisp . t)
-   (python     . t)
-   (shell      . t)
-   (latex      . t)))
-
-(defun org-babel-bash-initiate-session (&optional session _params)
-  "Initiate a bash/sh session named SESSION according to PARAMS."
-  (org-babel-sh-initiate-session session _params))
-
-(setq org-babel-python-command "python3")
-
-;; suppress security confirmation when evaluating code
-(setq org-confirm-babel-evaluate nil)
+(setq org-babel-default-header-args
+      '((:noweb   . "yes")
+        (:async   . "yes")
+        (:session . "none")
+        (:results . "replace")
+        (:exports . "code")
+        (:cache   . "no")
+        (:hlines  . "no")
+        (:tangle  . "no")))
 
 (provide 'shapes-module-org-babel)
 ;;; shapes-org-babel.el ends here
