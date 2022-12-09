6b97af41e895b04dff8b73b15a540565cb5d0160
alopezrivera <antonlopezr99@gmail.com>
2022-12-09 11:16:21 +0100

    m

--- persistent/custom.el
+++ persistent/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("47e0d1392558cdb3a1a73833f7609c526809e5859755c9042cc24aea6a8263c8" default))
  '(safe-local-variable-values '((org-use-property-inheritance . t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
