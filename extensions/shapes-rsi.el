75aa04f9691a2f290a13ecf34e8023ed31534e21
alopezrivera <antonlopezr99@gmail.com>
2023-01-04 13:13:56 +0100

    rsi - buffer switching

--- extensions/shapes-rsi.el
+++ extensions/shapes-rsi.el
@@ -2,5 +2,13 @@
 (global-set-key (kbd "C-=") #'save-buffer)
 (global-set-key (kbd "C-x C-s") nil)
 
+;; buffer switching
+(cl-loop for map in (list
+                     global-map
+                     ;; plus minor mode maps that override the keys
+                     pixel-scroll-precision-mode-map)
+         do (define-key map (kbd "<next>")  #'next-buffer)
+         do (define-key map (kbd "<prior>") #'previous-buffer))
+
 (provide 'shapes-extension-rsi)
 ;;; shapes-rsi.el ends here
