582578580f168f0c6edfc3bbcbf2ae99ed22b716
alopezrivera <antonlopezr99@gmail.com>
2023-01-07 09:08:43 +0100

    org-pomodoro visual notifications

--- modules/shapes-org-pomodoro.el
+++ modules/shapes-org-pomodoro.el
@@ -1,5 +1,8 @@
 ;; org-pomodoro
 (straight-use-package 'org-pomodoro)
 
+;; visual notifications
+(setq alert-user-configuration (quote ((((:category . "org-pomodoro")) libnotify nil))))
+
 (provide 'shapes-module-org-pomodoro)
 ;;; shapes-org-pomodoro.el ends here
