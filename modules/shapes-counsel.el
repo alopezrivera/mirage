fe4094bbba5acc575d954a6c9f923853e23b8066
alopezrivera <antonlopezr99@gmail.com>
2023-01-08 22:18:52 +0100

    M-x -> counsel-M-x

--- modules/shapes-counsel.el
+++ modules/shapes-counsel.el
@@ -1,7 +1,7 @@
 (straight-use-package 'counsel)
 (require 'counsel)
 
-(global-set-key (kbd "<menu>") #'counsel-M-x)
+(global-set-key (kbd "M-x") #'counsel-M-x)
 
 (provide 'shapes-module-counsel)
 ;;; shapes-counsel.el ends here
