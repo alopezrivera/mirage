c61a4e0d2b7c76891fb81be79b79c2669a6fd24e
alopezrivera <antonlopezr99@gmail.com>
2023-01-09 01:09:21 +0100

    horizontal scrolling bindings

--- layers/shapes-navigation.el
+++ layers/shapes-navigation.el
@@ -3,6 +3,12 @@
 (setq auto-window-vscroll nil)
 (setq mouse-wheel-progressive-speed nil)
 
+;; horizontal scrolling
+(put 'scroll-left 'disabled nil)
+(put 'scroll-right 'disabled nil)
+(global-set-key (kbd "S-<wheel-up>")   (lambda () (interactive) (scroll-left 2)))
+(global-set-key (kbd "S-<wheel-down>") (lambda () (interactive) (scroll-right 2)))
+
 ;; buffers
 (global-set-key (kbd "M-<delete>") #'bury-buffer)
 
