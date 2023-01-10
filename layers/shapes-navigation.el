327c6a010ee06ac102ff1973fb34253f6a4a7226
alopezrivera <antonlopezr99@gmail.com>
2023-01-10 15:14:12 +0100

    mouse side wheel horizontal scrolling

--- layers/shapes-navigation.el
+++ layers/shapes-navigation.el
@@ -4,10 +4,16 @@
 (setq mouse-wheel-progressive-speed nil)
 
 ;; horizontal scrolling
-(put 'scroll-left 'disabled nil)
+(put 'scroll-left  'disabled nil)
 (put 'scroll-right 'disabled nil)
-(global-set-key (kbd "S-<wheel-up>")   (lambda () (interactive) (scroll-right 2)))
-(global-set-key (kbd "S-<wheel-down>") (lambda () (interactive) (scroll-left  2)))
+;; mouse wheel
+(global-set-key (kbd "S-<wheel-up>")    (lambda () (interactive) (scroll-right 2)))
+(global-set-key (kbd "S-<wheel-down>")  (lambda () (interactive) (scroll-left  2)))
+;; mouse side wheel
+(global-set-key (kbd "<wheel-left>")    (lambda () (interactive) (scroll-right 8)))
+(global-set-key (kbd "<wheel-right>")   (lambda () (interactive) (scroll-left  8)))
+(global-set-key (kbd "S-<wheel-left>")  (lambda () (interactive) (scroll-right 4)))
+(global-set-key (kbd "S-<wheel-right>") (lambda () (interactive) (scroll-left  4)))
 
 ;; buffers
 (global-set-key (kbd "M-<delete>") #'bury-buffer)
