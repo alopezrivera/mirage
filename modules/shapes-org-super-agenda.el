d23952d35921101eab7ca54ec41ef5c54218d408
alopezrivera <antonlopezr99@gmail.com>
2023-02-12 12:32:05 +0100

    load org-super-agenda :before org-agenda

--- modules/shapes-org-super-agenda.el
+++ modules/shapes-org-super-agenda.el
@@ -2,7 +2,7 @@
 (require 'org-super-agenda)
 
 ;; advice
-(advice-add 'org-agenda :after (lambda (&rest args) (org-super-agenda-mode 1)))
+(advice-add 'org-agenda :before (lambda (&rest args) (org-super-agenda-mode 1)))
 
 (provide 'shapes-module-org-super-agenda)
 ;;; shapes-org-super-agenda.el ends here
